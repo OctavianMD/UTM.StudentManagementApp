9d96c3d66c5ae867debf039181295b4854e53b8c
Octavian Godonoga <octaviangodonoga@gmail.com>
2021-10-06 09:27:49 +0300

    Change db context

--- DataLayer/AppDbContext.cs
+++ DataLayer/AppDbContext.cs
@@ -22,29 +22,29 @@ namespace DataLayer
                 entity.ToTable("Class");
                 entity.HasKey(x => x.Id);
                 entity.Property(x => x.Name).HasMaxLength(10);
-                entity.HasMany<Course>();
-                entity.HasMany<Student>();
+                entity.HasMany(x => x.Courses);
+                entity.HasMany(x => x.Students);
             });
 
             modelBuilder.Entity<Student>(entity =>
             {
                 entity.HasKey(x => x.Id);
                 entity.HasIndex(x => x.Idnp);
-                entity.HasOne<ClassRoom>();
+                entity.HasOne(x => x.ClassRoom);
             });
 
             modelBuilder.Entity<Course>(entity =>
             {
                 entity.HasKey(x => x.Id);
-                entity.HasOne<Teacher>();
-                entity.HasMany<Student>();
-                entity.HasMany<ClassRoom>();
+                entity.HasOne(x => x.Teacher);
+                entity.HasMany(x => x.Students);
+                entity.HasMany(x => x.ClassRooms);
             });
 
             modelBuilder.Entity<Teacher>(entity =>
             {
                 entity.HasKey(x => x.Id);
-                entity.HasMany<Course>();
+                entity.HasMany(x => x.Courses);
             });
         }
     }
--- DataLayer/Migrations/20211006062543_Initial.Designer.cs
+++ DataLayer/Migrations/20211006062543_Initial.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataLayer.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20211006055146_Initial")]
+    [Migration("20211006062543_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
